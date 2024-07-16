3271a0c8d495bd4079f7dbb43a16bce704c29394
JMSONNE <jordynmsonne@gmail.com>
2024-07-16 16:03:11 -0500

    added shopping cart to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,6 +8,9 @@ import Toolbar from '@mui/material/Toolbar';
 import { useNavigate } from 'react-router-dom'
 import '../index.css';
 import { Margin } from '@mui/icons-material';
+import { Icon } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Grid from '@mui/material/Grid';
 
 const Navbar = () => {
 
@@ -16,23 +19,42 @@ const Navbar = () => {
     const handleLogoClick = () => {
         navigate('/');
     }
+    const handleLogoCartClick = () => {
+        navigate('/');
+    }
 
     return (
         <>
             <AppBar position='static' color='success'>
-                <Toolbar>
-                    <IconButton
-                        size='large'
-                        edge='start'
-                        disableRipple='true'
-                        onClick={handleLogoClick}
-                    >
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="Javascripts Coffee Logo" className='logo' />
-                    </IconButton>
-                    <Typography variant='h6' className='companyNameBar' mt={3}>
-                        JavaScripts Coffee Co.
-                    </Typography>
-                </Toolbar>
+                <Grid container spacing={2} justifyContent="space-between" alignItems="center">
+                    <Grid item xs={10}>
+                        <Toolbar>
+                            <IconButton
+                                size='large'
+                                edge='start'
+                                disableRipple='true'
+                                onClick={handleLogoClick}
+                            >
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="Javascripts Coffee Logo" className='logo' />
+                            </IconButton>
+                            <Typography variant='h6' className='companyNameBar' mt={3} noWrap="false">
+                                JavaScripts Coffee Co.
+                            </Typography>
+                        </Toolbar>
+                    </Grid>
+                    <Grid item xs={2} alignItems="center">
+                        <Toolbar sx={{ justifyContent: "end" }}>
+                            <IconButton
+                                size='large'
+                                edge='end'
+                                disableRipple='true'
+                                onClick={handleLogoCartClick}
+                            >
+                                <ShoppingCartIcon sx={{ color: 'white' }} fontSize='large' />
+                            </IconButton>
+                        </Toolbar>
+                    </Grid>
+                </Grid>
             </AppBar >
         </>
     )
