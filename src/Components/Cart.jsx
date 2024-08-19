c9590ab63a1698d5a59335a46f2a6f6789bb0ac1
JMSONNE <jordynmsonne@gmail.com>
2024-08-18 20:07:23 -0500

    removed faulty jwt decode from productcard.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import jwtdecode, { jwtDecode } from 'jwt-decode'
+
 
 const Cart = () => {
 
-    const token = localStorage.getItem('token');
-    const decodedToken = jwtDecode(token);
-    const userId = decodedToken.id;
 
     return (
         <div>Cart</div>
